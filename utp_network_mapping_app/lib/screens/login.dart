5507f28363d4d33ce77e357a79b3e218b5c3eb7b
Rodolfo Campos <rodolfojscampos@gmail.com>
2022-09-26 04:34:02 -0300

    Add login screen

--- utp_network_mapping_app/lib/screens/login.dart
+++ utp_network_mapping_app/lib/screens/login.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
-  const Login({ Key? key }) : super(key: key);
+  const Login({Key? key}) : super(key: key);
 
   @override
   _LoginState createState() => _LoginState();
@@ -10,8 +10,57 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 0, 82, 168),
+        title: Image.asset('assets/logo-itanet-1.png', fit: BoxFit.cover),
+      ),
+      body: Center(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.fromLTRB(30, 20, 30, 0),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(90.0),
+                ),
+                labelText: 'Login',
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(30, 20, 30, 0),
+            child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(90.0),
+                ),
+                labelText: 'Senha',
+              ),
+            ),
+          ),
+          Container(
+              height: 80,
+              width: MediaQuery.of(context).size.width,
+              padding: const EdgeInsets.fromLTRB(30, 20, 30, 0),
+              child: ElevatedButton(
+                style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(90.0),
+                ))),
+                child: const Text(
+                  'Entrar',
+                  style: TextStyle(fontSize: 18),
+                ),
+                onPressed: () {},
+              )),
+        ],
+      )),
     );
   }
 }
